df0dd7e321686eee2596c46d72ec710826b4607d
Jack Tan <Jack.Tan@airnz.co.nz>
2019-09-04 17:13:50 +1200

    init project

---
+++ src/gameRecorder.js
@@ -0,0 +1,79 @@
+const constraints = { audio: true, video: true };
+navigator.mediaDevices
+  .getDisplayMedia(constraints)
+  .then(function(stream) {
+    const video = document.querySelector('video');
+    video.srcObject = stream;
+
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.start();
+
+    console.log(mediaRecorder);
+    setTimeout(() => {
+      // mediaRecorder.requestData();
+      mediaRecorder.stop();
+    }, 6000);
+    console.log(mediaRecorder);
+    let chunks = [];
+    mediaRecorder.ondataavailable = event => {
+      console.log(event.data);
+      chunks.push(event.data);
+      // console.log(chunks);
+      // console.log(new Blob(chunks, { type: "video/webm"}));
+    };
+
+    mediaRecorder.addEventListener('stop', () => {
+      const recording = new Blob(chunks, {
+        type: 'audio/mpeg',
+      });
+
+      const blobUrl = URL.createObjectURL(recording);
+      console.log(recording, blobUrl);
+      const anchor = document.createElement('a');
+      anchor.setAttribute('href', blobUrl);
+      const now = new Date();
+      anchor.setAttribute(
+        'download',
+        `recording-${now.getFullYear()}-${(now.getMonth() + 1)
+          .toString()
+          .padStart(2, '0')}-${now
+          .getDay()
+          .toString()
+          .padStart(2, '0')}--${now
+          .getHours()
+          .toString()
+          .padStart(2, '0')}-${now
+          .getMinutes()
+          .toString()
+          .padStart(2, '0')}-${now
+          .getSeconds()
+          .toString()
+          .padStart(2, '0')}.webm`
+      );
+      anchor.innerText = 'Download';
+      document.body.appendChild(anchor);
+
+      const video = document.createElement('audio');
+      const source = document.createElement('source');
+      video.setAttribute('controls', '');
+      video.setAttribute('width', 600);
+      source.setAttribute('src', blobUrl);
+      source.setAttribute('type', 'audio/mpeg');
+
+      video.appendChild(source);
+      document.body.appendChild(video);
+    });
+
+    video.onloadedmetadata = function(e) {
+      video.play();
+    };
+  })
+  .catch(function(err) {
+    console.log('Error in getting stream', err);
+  });
+
+console.log('test');
+
+fetch('http://localhost:8080/login')
+  .then(res => res.json())
+  .then(res => console.log(res));
